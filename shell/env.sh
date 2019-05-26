2194dc84647ea4ab248568462ac6813b8ec9d628
Mael Rimbault <2587753+mrimbault@users.noreply.github.com>
2019-05-26 23:28:44 +0200

    Modify stdout layout when calling ansible scripts.

--- shell/env.sh
+++ shell/env.sh
@@ -7,6 +7,7 @@ export VIEWER=view
 # Define default terminal for i3-sensible-terminal
 export TERMINAL="termite"
 
-
+# Modify stdout layout when calling ansible scripts.
+export ANSIBLE_STDOUT_CALLBACK="yaml"
 
 
